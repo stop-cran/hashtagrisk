53887df3cdb8293b2b87fbf1b75ff671866117e7
stop-cran <stop-cran@list.ru>
2016-12-25 14:12:24 +0200

    Added mock project.

---
+++ RiskApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RiskApp.Startup))]
+namespace RiskApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
